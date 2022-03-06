962fbe8db6340d89ee171c6ebe7b314957ebdeba
Vincent Hiribarren <vhiribarren@users.noreply.github.com>
2022-03-06 21:06:12 +0100

    Refacto

--- sphinx_reqlist/__init__.py
+++ sphinx_reqlist/__init__.py
@@ -1,35 +1,51 @@
 from docutils import nodes
 from docutils.nodes import Node
-from typing import Sequence, cast, List
+from typing import Sequence, cast, List, Dict
 from sphinx.util.docutils import SphinxDirective
 from sphinx import addnodes
 
 
 class ReqDirective(SphinxDirective):
+    has_content = True
+    required_arguments = 1
+    final_argument_whitespace = True
 
-   has_content = True
-   required_arguments = 1
-   final_argument_whitespace = True
+    def run(self) -> Sequence[Node]:
 
-   def run(self) -> Sequence[Node]:
-       req_desc_signature = addnodes.desc_signature()
-       req_desc_signature += addnodes.desc_name(text=self.arguments[0])
-       req_desc_content = addnodes.desc_content()
-       self.state.nested_parse(self.content, self.content_offset, req_desc_content)
-       self.show_fields(req_desc_content)
-       req_desc = addnodes.desc()
-       req_desc += req_desc_signature
-       req_desc += req_desc_content
-       return [req_desc]
+        title = self.arguments[0]
 
-   def show_fields(self, desc_content):
-       for node in desc_content:
-           if not isinstance(node, nodes.field_list):
-               continue
-           fields = cast(List[nodes.field], node)
-           for field in fields:
-               field_name = cast(nodes.field_name, field[0]).astext().strip()
-               print(field_name)
+        target_id = f"req-{self.env.new_serialno('req')}"
+        target_node = nodes.target('', '', ids=[target_id])
+
+        req_desc_signature = addnodes.desc_signature()
+        req_desc_signature += addnodes.desc_name(text=title)
+        req_desc_content = addnodes.desc_content()
+        self.state.nested_parse(self.content, self.content_offset, req_desc_content)
+        req_desc = addnodes.desc()
+        req_desc += req_desc_signature
+        req_desc += req_desc_content
+
+        if not hasattr(self.env, 'req_all_reqs'):
+            self.env.req_all_reqs = []
+        self.env.req_all_reqs.append({
+            "title": title,
+            "attributes": self.extract_attributes(req_desc_content),
+            "target": target_node
+        })
+
+        return [target_node, req_desc]
+
+    def extract_attributes(self, desc_content: addnodes.desc_content) -> Dict[str, str]:
+        attributes: Dict[str, str] = {}
+        for node in desc_content:
+            if not isinstance(node, nodes.field_list):
+                continue
+            fields = cast(List[nodes.field], node)
+            for field in fields:
+                field_name = cast(nodes.field_name, field[0]).astext().strip()
+                field_body = cast(nodes.field_body, field[1]).astext().strip()
+                attributes[field_name] = field_body
+        return attributes
 
 
 def setup(app):
